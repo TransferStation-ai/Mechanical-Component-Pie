ec60e2d265beef9446bc1082dc215a96e5089dc0
TransferStation-ai <3997290192@qq.com>
2026-04-21 18:10:09 +0800

    Add files via upload
    新增了齿轮的配方

--- src/main/java/item/PartPie.java
+++ src/main/java/item/PartPie.java
@@ -1,3 +1,13 @@
+package item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import java.util.Random;
 
 
@@ -32,4 +42,5 @@ public class PartPie extends Item {
         return result;
     }
 }
+
 
